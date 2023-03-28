ff3c4fd7dd943b3bd471061688121660b1236c28
Maxim Wilman <mwilman@brockhaus-ag.de>
2023-03-28 12:27:13 +0200

    added lambda

--- lib/node-app-cdk-stack.ts
+++ lib/node-app-cdk-stack.ts
@@ -1,7 +1,17 @@
-import {App, Stack, StackProps} from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib'
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
-export class NodeAppCdkStack extends Stack {
-  constructor(scope: App, id: string, props?: StackProps) {
-    super(scope, id, props);
-  }
+export class NodeAppCdkStack extends cdk.Stack {
+    constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        new lambda.Function(this, 'LambdaNodeStack', {
+            code: lambda.Code.fromAsset('./src'),
+            functionName: "lambdaNode",
+            handler: 'index.handler',
+            memorySize: 1024,
+            runtime: lambda.Runtime.NODEJS_14_X,
+            timeout: cdk.Duration.seconds(300),
+        });
+    }
 }
