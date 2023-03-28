7c470a639e63cda925d60fc393654fab0f88509d
Maxim Wilman <mwilman@brockhaus-ag.de>
2023-03-28 13:50:02 +0200

    changed the lambda a little bit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 exports.handler = async function(event) {
+    console.log("Hallo aus dem lambda");
+    const result = 42+404;
+    console.log("result")
+    console.log(result)
     return {
         statusCode: 200,
         headers: { "Content-Type": "text/json" },
-        body: JSON.stringify({ message: "Hello from my Lambda node!" })
+        body: JSON.stringify({ message: "Hello from my new Lambda node!"})
     };
 };
