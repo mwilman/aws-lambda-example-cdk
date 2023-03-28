2be237921a78179c364704a905b7d0681d2dd9b0
Maxim Wilman <mwilman@brockhaus-ag.de>
2023-03-28 12:05:44 +0200

    remove sample code

--- lib/node-app-cdk-stack.ts
+++ lib/node-app-cdk-stack.ts
@@ -1,19 +1,7 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import {App, Stack, StackProps} from 'aws-cdk-lib';
 
 export class NodeAppCdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: App, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'NodeAppCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'NodeAppCdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
