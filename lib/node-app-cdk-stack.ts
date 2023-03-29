f1ad4d084ef4e53dfd5d815738ecdd8ca69098c9
Maxim Wilman <mwilman@brockhaus-ag.de>
2023-03-29 15:13:48 +0200

    added db and http trigger

--- lib/node-app-cdk-stack.ts
+++ lib/node-app-cdk-stack.ts
@@ -1,12 +1,14 @@
 import * as cdk from 'aws-cdk-lib'
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import {NodejsFunction} from "aws-cdk-lib/aws-lambda-nodejs";
+import {AttributeType, Table} from "aws-cdk-lib/aws-dynamodb";
+import {LambdaIntegration, RestApi} from "aws-cdk-lib/aws-apigateway";
 
 export class NodeAppCdkStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        new NodejsFunction(this, 'LambdaTSFunction', {
+        const dynamoLambda = new NodejsFunction(this, 'LambdaTSFunction', {
             entry: 'src/index.ts',
             handler: 'index.handler',
             memorySize: 1024,
@@ -16,5 +18,19 @@ export class NodeAppCdkStack extends cdk.Stack {
             runtime: lambda.Runtime.NODEJS_14_X,
             timeout: cdk.Duration.seconds(300),
         });
+
+        const table = new Table(this, "Hello", {
+            partitionKey: { name: "name", type: AttributeType.STRING },
+        });
+
+        // permissions to lambda to dynamo table
+        table.grantReadWriteData(dynamoLambda);
+
+        // create the API Gateway with one method and path
+        const api = new RestApi(this, "hello-api");
+
+        api.root
+            .resourceForPath("hello")
+            .addMethod("GET", new LambdaIntegration(dynamoLambda));
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 
 export const handler = async (event: {key1: string, key2: string, key3:string}) => {
-    console.log("Hallo aus dem lambda");
-    const result = 42+404;
-    console.log("result")
-    console.log(result)
-    console.log("request:", JSON.stringify(event, undefined, 2));
+    console.log("request:", JSON.stringify(event));
 
+    // return response back to upstream caller
+    return sendRes(200, "HELLLOOO");
+};
+
+const sendRes = (status: number, body: string) => {
     return {
-        statusCode: 200,
-        headers: { "Content-Type": "text/plain" },
-        body: `Hello, CDK! key1 is: ${event.key1}\n`
+        statusCode: status,
+        headers: {
+            "Content-Type": "text/html",
+        },
+        body: body,
     };
 };
